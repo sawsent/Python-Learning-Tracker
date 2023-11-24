de568d5ae671ce5dbd7efaed8a1d0172fca7b88c
Vicente Figueiredo <vicenteppfigueiredo@gmail.com>
2023-11-24 19:15:18 +0000

    tried to apply a little more encapsulation

--- OOP/Class1/Project/account.py
+++ OOP/Class1/Project/account.py
@@ -19,12 +19,16 @@ class Account:
 
     def __init__(self, name: str, starting_value=0):
         self.name = name
-        self.value = starting_value  # the name can be changed, but the value can only be changed through the funcs
+        self.__value = starting_value  # the name can be changed, but the value can only be changed through the funcs
 
         self.all.append(self)
-        
+    
+    @property
+    def value(self):
+        return self.__value
+    
     def get_details(self):
-        return {'name': self.name, 'value': self.value}
+        return {'name': self.name, 'value': self.__value}
     
     def get_readable_details(self):
         details = self.get_details()
@@ -40,29 +44,29 @@ class Account:
                 break
             else: print("Can't deposit negative amounts of money!")
 
-        self.value += amount_to_deposit
-        print(f"Successfully deposited {amount_to_deposit}€, your new balance is {self.value}€!")
+        self.__value += amount_to_deposit
+        print(f"Successfully deposited {amount_to_deposit}€, your new balance is {self.__value}€!")
 
     def add_money(self, money: float):
-        self.value += round(money, 2)
+        self.__value += round(money, 2)
 
     def remove_money(self, money: float):
-        self.value -= round(money, 2)
+        self.__value -= round(money, 2)
 
     def withdraw(self):
         while True:
             user_input = input("How much do you want to withdraw?\n»» ")
             if user_input == 'cancel': return
             amount_to_withdraw = round(float(user_input), 2)
-            if amount_to_withdraw <= self.value and amount_to_withdraw > 0:
+            if amount_to_withdraw <= self.__value and amount_to_withdraw > 0:
                 break
-            else: print(f"Can't withdraw more money than your current balance or negative money: {self.value}€")
+            else: print(f"Can't withdraw more money than your current balance or negative money: {self.__value}€")
             
-        self.value -= amount_to_withdraw
-        print(f"Successfully withdrew {amount_to_withdraw}€, your new balance is {self.value}€!")
+        self.__value -= amount_to_withdraw
+        print(f"Successfully withdrew {amount_to_withdraw}€, your new balance is {self.__value}€!")
 
     def destroy(self):
         self.all.remove(self)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.value})"
+        return f"{self.__class__.__name__}('{self.name}', {self.__value})"
